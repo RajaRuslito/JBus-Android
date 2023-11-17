fa7b11bd0c0d414b42dbf77e4e3f06a9cacffff7
RajaRuslito <litoraja123@gmail.com>
2023-11-17 11:42:57 +0700

    CS08 - Menyelesaikan CS Modul 8

--- app/src/main/java/com/RajaYonandroRuslitoJBusAF/jbus_android/LoginActivity.java
+++ app/src/main/java/com/RajaYonandroRuslitoJBusAF/jbus_android/LoginActivity.java
@@ -2,13 +2,25 @@ package com.RajaYonandroRuslitoJBusAF.jbus_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView registerNow = null;
+    //private Button loginButton = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        registerNow = findViewById(R.id.register_now);
+        //loginButton = findViewById(R.id.login_button);
     }
+    private void moveActivity(Context ctx, Class<?> cls){
+        Intent intent = new Intent(ctx, cls);
+    }
+
+
 }
